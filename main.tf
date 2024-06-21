9cd418b1910c4b023a4daad4fc8ba5d280705c0e
hashikoenig <144815617+hashikoenig@users.noreply.github.com>
2024-06-21 17:07:14 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,9 @@
+//--------------------------------------------------------------------
+// Modules
+module "s3_bucket" {
+  source  = "app.terraform.io/TerraformAWSexample/s3-bucket/aws"
+  version = "3.15.1"
+
+  bucket = "bucketname"
+  bucket_prefix = "bucketname"
+}
