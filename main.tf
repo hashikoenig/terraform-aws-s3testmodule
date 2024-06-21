927e7b3f91e33c817e8dcba6ad1431b75ea8a22b
hashikoenig <144815617+hashikoenig@users.noreply.github.com>
2024-06-21 17:22:57 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,6 @@ module "s3_bucket" {
   source  = "app.terraform.io/TerraformAWSexample/s3-bucket/aws"
   version = "3.15.1"
 
-  bucket = "bucketname"
+  bucket = ${bucketname}
   bucket_prefix = "bucketname"
 }
